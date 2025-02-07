551324e16fd53e1ed3de46bae1b1c271953862fe
Utsav <utsavkhatri7@gmail.com>
2025-02-07 14:41:07 +1100

    first Commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect,useState} from 'react';
 
+import axios from 'axios';
 export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://192.168.1.222:5000/")
+      .then(response => setMessage(response.data))
+      .catch(error => console.error(error));
+  });
   return (
+    
     <View style={styles.container}>
+      
+      <Text>{message}</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
