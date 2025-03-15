94431c8151484f0efa38ec54f1889c88969b4297
Pranish <iampranish@outlook.com>
2025-03-16 01:57:27 +1100

    Add : Server, Channel api

---
+++ Src/api/server/channelApi.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError } from "axios";
+import { ApiError, ApiResponse } from "../utils/apiResponse";
+
+export interface getAllChannelForCurrentServerResponse {
+  id: string;
+  name: string;
+}
+export const API = axios.create({
+  baseURL:
+    // "https://8c1f-2406-2d40-4d55-6c10-bdc3-9abf-864e-c64f.ngrok-free.app",
+    "http://localhost:3000/api/v1/",
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const getAllChannelForCurrentServer = async (serverId: string) => {
+  try {
+    const response = await API.get("channel/getAllChannelForCurrentServer", {
+      params: { serverId },
+    });
+
+    const res =
+      response.data as ApiResponse<getAllChannelForCurrentServerResponse>;
+    return res;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      const err = error.response?.data as ApiError<{}>;
+      return new ApiError(err.statusCode, {}, err.message);
+    } else {
+      return new ApiError(400, {}, "Something went wrong");
+    }
+  }
+};
