ff2113ddd9669b4e279db8ba543d95ef416f7a5f
euronymousutsv <utsavkhatri7@gmail.com>
2025-04-17 14:29:05 +1000

    build

--- src/types/Channel.ts
+++
@@ -1,8 +0,0 @@
-export interface Channel {
-    id: string;
-    name: string;
-    newMessages: number;
-    isPrivate: boolean;
-    highestRoleToAccessChannel?: 'admin' | 'manager' | 'employee';
-  }
-  
--- src/utils/color.ts
+++
@@ -1,6 +0,0 @@
-export const PrimaryColor = "#4A90E2";
-export const AccentColor = "#2ECC71";
-export const BackgroundColor = "#FDFDFF";
-export const TextColor = "#393D3F";
-export const ButtonRed = "#D9534F";
-export const ActiveTabColor = "#88B6EC";
